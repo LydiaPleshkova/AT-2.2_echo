4378172da4a5df09d4d01b855d31689e945bfae0
Lydia Pleshkova <lydia.y.pleshkova@gmail.com>
2023-07-12 20:23:23 +0300

    test passed

--- src/test/java/ru/netology/echo/postmanEchoTest.java
+++ src/test/java/ru/netology/echo/postmanEchoTest.java
@@ -2,6 +2,9 @@ package ru.netology.echo;
 
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
 public class postmanEchoTest {
 
     @Test
